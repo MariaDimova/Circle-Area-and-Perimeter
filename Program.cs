ff618e23880a7d89c515d1c9b3cee5a5a5047571
MariaDimova <150257957+MariaDimova@users.noreply.github.com>
2023-11-19 18:00:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Area_and_Perimeter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // чете от конзолата число r и пресмята и отпечатва лицето S и периметъра P на кръг
+           //  окръжност с радиус r.
+            double r = double.Parse(Console.ReadLine());
+            double area = Math.PI * r * r;
+            double perimeter = Math.PI * 2 * r;
+
+            Console.WriteLine(area);
+            Console.WriteLine(perimeter);
+        }
+    }
+}
